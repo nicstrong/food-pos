eb79eb328385da221dcb41a70209976dac555b20
Nic Strong <nic.strong@ipfx.com>
2023-08-22 22:32:08 +1200

    Added react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-right"/>
       <Component {...pageProps} />
     </ClerkProvider>
   );
